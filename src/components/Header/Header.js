89663642018e042778f959eaaeedfd1964aaa8fc
Sergey Yarkov <serzh.yarkov@gmail.com>
2020-07-20 15:14:02 +0300

    fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,13 +14,14 @@ const Header = () => {
   const { siteMetadata: { title, menuLinks, social: { twitter, telegram } } } = useSiteMetadataQuery()
   const [isScrolled, setIsScrolled] = React.useState(false)
   const [isMobileOpen, setIsMobileOpen] = React.useState(false)
+  const [isDark, setIsDark] = React.useState(false)
   const { pathname } = useLocation()
 
   React.useEffect(() =>  {
-    const [isDark, setIsDark] = React.useState(window.__isDarkTheme)
+    setIsDark(window.__isDarkTheme)
     isMobileOpen ? document.body.style.overflowY = 'hidden' : document.body.style.overflowY = ''
     document.body.onscroll = () => window.pageYOffset >= 100 ? setIsScrolled(true) : setIsScrolled(false)
-  })
+  }, [setIsDark, isMobileOpen])
 
   const mobileHandler = () => {
     setIsMobileOpen(!isMobileOpen)
