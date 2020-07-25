1746ae6a7b40bc27009c2744d47ef61d3982eee5
Sergey Yarkov <serzh.yarkov@gmail.com>
2020-07-25 14:17:49 +0300

    gatsby config images

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    `gatsby-plugin-sitemap`,
     "gatsby-plugin-sass",
     {
       resolve: "gatsby-source-filesystem",
@@ -24,6 +23,22 @@ module.exports = {
         name: "assets",
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/content`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/,
+        },
+      },
+    },
+    "gatsby-plugin-sitemap",
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
@@ -96,61 +111,53 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-relative-images`,
+            resolve: `gatsby-plugin-netlify-cms-paths`,
             options: {
-              name: "assets",
+              cmsConfig: `/static/admin/config.yml`,
             },
           },
           {
             resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 2400,
+              quality: 100,
+              withWebp: true,
+              loading: 'lazy',
+              linkImagesToOriginal: false,
+            }
+          },
+          `gatsby-remark-responsive-iframe`,
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
           },
           {
-            resolve: `gatsby-transformer-remark`,
+            resolve: `gatsby-remark-autolink-headers`,
             options: {
-              plugins: [
-                {
-                  resolve: `gatsby-remark-autolink-headers`,
-                  options: {
-                    offsetY: `140`,
-                    icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path class="anchor-icon" fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
-                    className: `anchor-link`,
-                    maintainCase: true,
-                    removeAccents: true,
-                    isIconAfterHeader: true,
-                    elements: [`h1`, `h2`, `h3`, `h4`],
-                  },
+              offsetY: `140`,
+              icon: `<svg aria-hidden="true" height="20" version="1.1" viewBox="0 0 16 16" width="20"><path class="anchor-icon" fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>`,
+              className: `anchor-link`,
+              maintainCase: true,
+              removeAccents: true,
+              isIconAfterHeader: true,
+              elements: [`h1`, `h2`, `h3`, `h4`],
+            },
+          },
+          {
+            resolve: `gatsby-remark-vscode`,
+            options: {
+              extensions: ["material-theme"],
+              theme: {
+                default: "Light+ (default light)",
+                parentSelector: {
+                  "body.dark": "One Dark Pro",
                 },
-                {
-                  resolve: `gatsby-remark-vscode`,
-                  options: {
-                    extensions: ["material-theme"],
-                    theme: {
-                      default: "Light+ (default light)",
-                      parentSelector: {
-                        "body.dark": "One Dark Pro",
-                      },
-                    },
-                  },
-                },
-              ],
+              },
             },
           },
         ],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /images/,
-        },
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "content",
-        path: `${__dirname}/content`,
       },
     },
     "gatsby-plugin-netlify-cms",
