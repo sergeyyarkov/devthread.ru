4d2db20caf5a7dd4714f33742fe14e4b6cf8a70e
Sergey Yarkov <serzh.yarkov@gmail.com>
2020-08-17 15:40:43 +0300

    articles more images quality

--- src/hooks/useMoreArticlesQuery.js
+++ src/hooks/useMoreArticlesQuery.js
@@ -17,7 +17,7 @@ const useMoreArticlesQuery = () => {
               title
               image {
                 childImageSharp {
-                  fluid {
+                  fluid(maxWidth: 650 quality: 100) {
                     ...GatsbyImageSharpFluid
                   }
                 }
