3c19dfad04d22eeabda10f1a1c7f7e1fb924ebb1
Sergey Yarkov <serzh.yarkov@gmail.com>
2021-01-14 19:17:32 +0300

    move styles from gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,21 +4,7 @@ import Layout from "./src/components/Layout/Layout"
 
 import "prismjs/themes/prism-tomorrow.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
-
 import "./src/styles/global.scss"
-import "./src/components/Header/Header.scss"
-import "./src/components/About/About.scss"
-import "./src/components/Offers/Offers.scss"
-import "./src/components/Articles/Articles.scss"
-import "./src/components/Articles/ArticleCard/ArticleCard.scss"
-import "./src/components/Newsletter/Newsletter.scss"
-import "./src/components/Article/Article.scss"
-import "./src/components/Snippet/Snippet.scss"
-import "./src/components/Snippets/Snippets.scss"
-import "./src/components/Articles/MoreArticles/MoreArticles.scss"
-import "./src/components/Books/Books.scss"
-import "./src/components/Contacts/Contacts.scss"
-import "./src/components/Footer/Footer.scss"
 
 export const shouldUpdateScroll = ({ routerProps: { location } }) => {
   const { pathname } = location
